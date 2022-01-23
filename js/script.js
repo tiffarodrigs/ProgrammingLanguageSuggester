61c343c5fc3336f9aade001fa3afab0829e070b6
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-22 22:01:06 -0800

    Update indentation and spacing based on review comments

--- js/script.js
+++ js/script.js
@@ -1,57 +1,33 @@
-$("document").ready(function(){
-  $("form#suggester").submit(function(event){
+$("document").ready(function() {
+  $("form#suggester").submit(function(event) {
     event.preventDefault();
     let name = $("input#name").val();
+    let experience = $("#experience").val();
+    let goal = $('input[name="goal"]:checked').val();
+    let decision = $("#decision").val();
+    let hours_commit = $("#hours_commit").val();
+    let duration = $("#duration").val();
+    let output = "";
 
-    let experience= $( "#experience" ).val();
-    let goal= $('input[name="goal"]:checked').val();
-    let decision=$( "#decision" ).val();
-
-    let hours_commit= $( "#hours_commit" ).val();
-    let duration=$( "#duration" ).val();
-    let output="";
-   
-
-    if(experience==="none")
-    {
-      if(goal==="UX-designer")
-      {
-        if(decision="hype_about")
-        {
-          output= name+", HTML/CSS would be good choice to begin with";
+    if (experience === "none") {
+      if (goal === "UX-designer") {
+        if (decision = "hype_about") {
+          output = name + ", HTML/CSS would be good choice to begin with";
+        } else {
+          output = name + ", JavaScript would be a good choice to start with";
         }
-        else
-        {
-          output= name+", JavaScript would be a good choice to start with";
-        }
+      } else {
+        output = name + ", JavaScript would be a good choice to start with";
       }
-      else{
-        output= name+", JavaScript would be a good choice to start with";
-
-      }
+    } else if ((experience === "some" && goal === "fullStack") || (experience === "good" && goal === "fullStack")) {
+      output = name + ", JavaScript to become a Full Stack Developer";
+    } else if (goal === "dataScientist" || duration === "5_months" || hours_commit === "More than 20 hrs a week" || decision === "implement_project") {
+      output = name + ", Python  would be a good choice for you";
+    } else {
+      output = name + ", JavaScript is versatile language which can be useful for beginner or experienced";
     }
-
-    else if((experience==="some" && goal==="fullStack")||(experience==="good" && goal==="fullStack")){
-      output= name+", JavaScript to become a Full Stack Developer";
-
-    }
-    else if (goal==="dataScientist" ||duration==="5_months" || hours_commit==="More than 20 hrs a week"|| decision==="implement_project")
-    {
-      output= name+", Python  would be a good choice for you";
-
-
-    }
-    else
-    {
-      output= name+", JavaScript is versatile language which can be useful for beginner or experienced";
-    }
-  
-
     $("#input").slideUp(400);
     $("h5").hide();
     $("#outputdiv").text(output).slideDown();
- 
-
   });
-
 });
